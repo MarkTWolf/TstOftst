6cd06c9a29d36b0548caa5000e511a03579ec288
MarkTWolf <Marcos@21>
2018-07-20 01:11:19 -0300

    semstash2
    (cherry picked from commit 4c0ef55b12fca3dd9fb73cc4cd0f99f56bad8d83)
    
    # Conflicts:
    #	app/src/main/java/com/example/marcoscastro/myapplication/MainActivity.java

--- app/src/main/java/com/example/marcoscastro/myapplication/MainActivity.java
+++ app/src/main/java/com/example/marcoscastro/myapplication/MainActivity.java
@@ -10,5 +10,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
     }
 }
