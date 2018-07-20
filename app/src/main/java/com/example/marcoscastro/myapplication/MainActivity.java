216a28bdd17c486e3dcd2dc656ee47f5bccefd91
MarkTWolf <Marcos@21>
2018-07-20 00:44:18 -0300

    comstash2

--- app/src/main/java/com/example/marcoscastro/myapplication/MainActivity.java
+++ app/src/main/java/com/example/marcoscastro/myapplication/MainActivity.java
@@ -29,17 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
+
         setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
